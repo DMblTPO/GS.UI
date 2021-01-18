394aca2a2c1800929abce78c6eafc70ecab57ee5
Dmitry Boboshko <Dmitry.Boboshko@wolterskluwer.com>
2021-01-18 20:12:46 +0200

    (feat): design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterMenuComponent } from './components/footer-menu/footer-menu.component';
 import { GamesListComponent } from './components/games-list/games-list.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { GamesService } from './services/games-service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, GamesListComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [AppComponent, GamesListComponent, MainMenuComponent, FooterMenuComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
   providers: [GamesService],
   bootstrap: [AppComponent],
 })

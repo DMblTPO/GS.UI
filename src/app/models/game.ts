cdddaedff054bd374a0c06b8836ab4e7442280bd
Dmitry Boboshko <Dmitry.Boboshko@wolterskluwer.com>
2021-01-25 20:05:50 +0200

    fix: get game's genres

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -2,7 +2,7 @@ export class Game {
   id: number;
   name: string;
   description: string;
-  genre: string[];
+  genres: string[];
   pictureUri: string;
   pictureWideUri: string;
   price: number;
